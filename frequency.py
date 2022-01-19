639b083d1e4d92ca74946ac4bfeb35371ec816b6
sketch-imiss <imiss.sketch@gmail.com>
2022-01-20 00:16:32 +0800

    update frequency estimator

--- frequency.py
+++ frequency.py
@@ -1,5 +1,8 @@
 import logging
 import pickle
+import mmh3
+import random
+import math
 
 
 class Frequency:
@@ -17,9 +20,27 @@ class Frequency:
     def initialize(self):
         self.filter = [0] * self.filter_size
         self.cm_cu = [0] * (self.row * self.column)
+        self.f = 0
+        self.set_user = set()
 
     def basic_update(self):
-        print('basic_update')
+        with open(self.dataset) as freader:
+            for line in freader:
+                [user, weight] = list(map(int, line.strip().split()))
+                self.set_user.add(user)
+                lst_findex = [mmh3.hash(str(user) + '-' + str(i), signed=False) % self.filter_size
+                             for i in range(self.fhash)]
+                for _ in range(weight):
+                    gamma = min([self.filter[index] for index in lst_findex])
+                    if gamma < self.threshold:
+                        self.f += 1
+                        for i in range(self.fhash):
+                            random_num = math.floor(-math.log2(random.random()))
+                            self.filter[lst_findex[i]] = max(min(random_num, self.threshold), self.filter[lst_findex[i]])
+                    else:
+                        min_value = 2 ** 32 - 1
+                        min_index = 0
+
 
     def fast_update(self):
         print('fast_update')
